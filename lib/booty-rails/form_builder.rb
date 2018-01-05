547c0f2444d351633ff1d987a783742c5e313824
tayden-miller <tayden@ualberta.ca>
2018-01-05 15:03:44 -0700

    Patch.

--- lib/booty-rails/form_builder.rb
+++ lib/booty-rails/form_builder.rb
@@ -3,13 +3,8 @@ module BootyRails
   class FormBuilder < ActionView::Helpers::FormBuilder
 
     def initialize(object_name, object, template, options)
-      options[:html][:cool] = "yup"
-      puts "Hello!"
       super
     end
-
-
-
 
   end
 end
--- lib/booty-rails/helper.rb
+++ lib/booty-rails/helper.rb
@@ -2,13 +2,9 @@ module BootyRails
   module Helper
 
     def form_for(object, options = {}, &block)
-
       options.reverse_merge!({builder: BootyRails::FormBuilder})
-
       ActionView::Base.field_error_proc = proc { |input, instance| input }
-
       super(object, options, &block)
-
     end
 
   end
