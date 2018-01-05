b231856d73b523524b1846f171ac11030b9750aa
tayden-miller <tayden@ualberta.ca>
2018-01-05 14:51:07 -0700

    Patch.

--- lib/booty-rails.rb
+++ lib/booty-rails.rb
@@ -1,9 +1,14 @@
 require 'bootstrap'
 require 'booty-rails/form_builder'
+require 'booty-rails/helper'
 
 module BootyRails
   module Rails
     class Engine < ::Rails::Engine
     end
   end
+end
+
+ActiveSupport.on_load(:action_view) do
+  include BootyRails::Helper
 end
--- lib/booty-rails/form_builder.rb
+++ lib/booty-rails/form_builder.rb
@@ -1,22 +1,10 @@
 module BootyRails
 
-  def booty(object, options = {}, &block)
-    options.reverse_merge!({builder: BootyRails::FormBuilder})
-
-    ActionView::Base.field_error_proc = proc { |input, instance| input }
-
-    form_for(object, options, &block)
-
-  end
-
   class FormBuilder < ActionView::Helpers::FormBuilder
-    #extend BootstrapForm::Aliasing
-    #include BootstrapForm::Helpers::Bootstrap
-
-    #delegate :content_tag, :capture, :concat, to: :@template
 
     def initialize(object_name, object, template, options)
       puts "Hello!"
+      super
     end
 
 
