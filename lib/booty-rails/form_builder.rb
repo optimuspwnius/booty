c0b176a8b9c83bd529ed69b33c3f5f91a198d5c1
tayden-miller <tayden@ualberta.ca>
2018-01-05 16:23:48 -0700

    Patch.

--- lib/booty-rails/form_builder.rb
+++ lib/booty-rails/form_builder.rb
@@ -2,9 +2,15 @@ module BootyRails
 
   class FormBuilder < ActionView::Helpers::FormBuilder
 
+    delegate :content_tag, :capture, :concat, to: :@template
+
     def initialize(object_name, object, template, options)
       puts 'FormBuilder::initialize'
       super
+    end
+
+    def email_field(name, **options, &block)
+      super(name, options)
     end
 
   end
