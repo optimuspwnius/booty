303f3c599dcf860f394aacb161b039d833c0d75a
tayden.miller <tayden@ualberta.ca>
2018-01-05 13:15:35 -0700

    Patch

--- lib/booty/engine.rb
+++ lib/booty/engine.rb
@@ -1,7 +1,13 @@
 require 'bootstrap'
 
 module BootyRails
-  class Engine < ::Rails::Engine
 
+
+  class Engine < ::Rails::Engine
+    initializer 'booty_rails.initialize' do
+
+        require 'bootstrap'
+
+    end
   end
 end
