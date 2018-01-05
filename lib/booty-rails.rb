6ea05b56f654a474038ab00eb65cb319b431a17a
tayden-miller <tayden@ualberta.ca>
2018-01-05 15:30:01 -0700

    Patch.

--- lib/booty-rails.rb
+++ lib/booty-rails.rb
@@ -6,14 +6,18 @@ module BootyRails
     class Engine < ::Rails::Engine
     end
   end
+
+  module Helper
+    def form_for(object, options = {}, &block)
+      puts "Helper::form_for"
+      ActionView::Base.field_error_proc = proc { |input, instance| input }
+      super(object, options.reverse_merge!({builder: BootyRails::FormBuilder}), &block)
+    end
+  end
 end
 
 ActiveSupport.on_load(:action_view) do
 
-  def form_for(object, options = {}, &block)
-    puts "Helper::form_for"
-    ActionView::Base.field_error_proc = proc { |input, instance| input }
-    super(object, options.reverse_merge!({builder: BootyRails::FormBuilder}), &block)
-  end
+
 
 end
