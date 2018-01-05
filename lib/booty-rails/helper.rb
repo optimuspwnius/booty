ce2fa90de06905243c889247d60da4c6720b4057
tayden-miller <tayden@ualberta.ca>
2018-01-05 15:56:04 -0700

    Patch.

--- lib/booty-rails/helper.rb
+++ lib/booty-rails/helper.rb
@@ -1,10 +1,18 @@
 module BootyRails
   module Helper
+
     def form_for(object, options = {}, &block)
       puts "Helper::form_for"
       ActionView::Base.field_error_proc = proc { |input, instance| input }
       super(object, options.reverse_merge!({builder: BootyRails::FormBuilder}), &block)
     end
+
+    def form_with(model: nil, scope: nil, url: nil, format: nil, **options)
+      puts "Helper::form_with"
+      ActionView::Base.field_error_proc = proc { |input, instance| input }
+      super(model, scope, url, format, options.reverse_merge!({builder: BootyRails::FormBuilder}), &block)
+    end
+
   end
 end
 
