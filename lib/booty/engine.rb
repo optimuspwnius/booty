50e2bc7325383aadb8281b546229c02d7e46d502
tayden.miller <tayden@ualberta.ca>
2018-01-05 13:26:22 -0700

    Patch

--- lib/booty/engine.rb
+++ lib/booty/engine.rb
@@ -1,13 +1,19 @@
 require 'bootstrap'
 
 module BootyRails
-
-
+  
   class Engine < ::Rails::Engine
-    initializer 'booty_rails.initialize' do
+    config.booty_rails = BootyRails::Configuration.new
+    isolate_namespace BootyRails
 
-        require 'bootstrap'
+    initializer 'booty_rails.action_controller' do |app|
+      ActiveSupport.on_load(:action_controller) do
+        ::ActionController::Base.helper(BootyRails::ApplicationHelper)
+      end
+    end
 
+    initializer "booty_rails.form_builder" do
+      ActionView::Helpers::FormBuilder.send(:include, BootyRails::FormHelper)
     end
   end
 end
