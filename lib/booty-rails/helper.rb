8860bbd0f80611d505b676baab382dec35ed471d
tayden-miller <tayden@ualberta.ca>
2018-01-06 10:44:39 -0700

    Patch

--- lib/booty-rails/helper.rb
+++ lib/booty-rails/helper.rb
@@ -12,28 +12,12 @@ module BootyRails
       ActionView::Base.field_error_proc = proc { |input, instance| input }
 
       options.reverse_merge!({builder: BootyRails::FormBuilder})
+      puts 'Options:'
+      puts options
 
-      options[:allow_method_names_outside_object] = true
-      options[:skip_default_ids] = true
-
-      if options[:model]
-        options[:url] ||= polymorphic_path(options[:model], format: options[:format])
-
-        options[:model]   = options[:model].last if options[:model].is_a?(Array)
-        options[:scope] ||= model_name_from_record_or_class(options[:model]).param_key
+      super(options) do
+        capture(&Proc.new)
       end
-
-      if block_given?
-        builder = instantiate_builder(options[:scope], options[:model], options)
-        output  = capture(builder, &Proc.new)
-        options[:multipart] ||= builder.multipart?
-
-        html_options = html_options_for_form_with(options[:url], options[:model], options)
-        super(options) do
-          capture(&Proc.new)
-        end
-      end
-
 
     end
 
