dd051d9116aec66f751b596e55881eda48ead8e9
tayden-miller <tayden@ualberta.ca>
2018-01-06 11:25:19 -0700

    Patch

--- lib/booty-rails/helper.rb
+++ lib/booty-rails/helper.rb
@@ -16,7 +16,7 @@ module BootyRails
       puts options
 
       super(model: model, scope: scope, url: url, format: format, **options) do
-        capture(&Proc.new)
+        Proc.new.call
       end
 
     end
