79a46f854088d8d193a8d9c970dbfc3349143f62
tayden-miller <tayden@ualberta.ca>
2018-01-08 15:44:03 -0700

    Patch

--- lib/booty-rails/form_builder.rb
+++ lib/booty-rails/form_builder.rb
@@ -41,7 +41,7 @@ module BootyRails
       options.to_options!
       puts 'FormBuilder::control_options'
       _options = options.except(default_options)
-      options = options.except(_options)
+      options.except!(_options)
       puts _options
       _options
     end
