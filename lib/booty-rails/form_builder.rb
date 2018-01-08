44a6de6a671d6641b267570f46f4ad3b8817c04e
tayden-miller <tayden@ualberta.ca>
2018-01-08 16:04:22 -0700

    Patch

--- lib/booty-rails/form_builder.rb
+++ lib/booty-rails/form_builder.rb
@@ -41,7 +41,7 @@ module BootyRails
       options.to_options!
       puts 'FormBuilder::control_options'
       _options = options.except(default_options)
-      options.reverse_merge!(default_options)
+      options.slice!(default_options).reverse_merge!(default_options)
       puts _options
       _options
     end
